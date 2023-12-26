eb3b375fac862fc5a40483703f57e33d1fb063a8
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2023-12-27 01:33:05 +0200

    Final commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import styled from 'styled-components';
 import loginImg from '../images/login-img.svg';
-import { Redirect } from 'react-router-dom';
+
 
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
